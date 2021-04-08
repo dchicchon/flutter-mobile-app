9dedb012bc4f91147f47c7b240a6192487830d76
dchicchon <danielchicchon@gmail.com>
2021-04-07 17:33:35 -0700

    temporary fix on Photo.dart page with a Container

--- lib/Photo.dart
+++ lib/Photo.dart
@@ -13,27 +13,34 @@ class _PhotoState extends State<Photo> {
     return Scaffold(
         appBar: AppBar(),
         body: Container(
-          margin: EdgeInsetsDirectional.only(top: 50),
-          child: Column(children: [
-            Row(children: [
-              Text("Upload a photo?",
-                  style: TextStyle(
-                      fontSize: 25,
-                      color: Colors.black,
-                      fontWeight: FontWeight.w700)),
-            ]),
-            Container(
-                margin: EdgeInsetsDirectional.only(top: 50),
-                child: ElevatedButton(
-                    onPressed: this.submitPhoto,
-                    child: Text("Update", style: TextStyle(fontSize: 15)),
-                    style: ElevatedButton.styleFrom(
-                        primary: Colors.black,
-                        onPrimary: Colors.white,
-                        elevation: 10,
-                        shadowColor: Colors.black,
-                        padding: EdgeInsets.fromLTRB(75, 15, 75, 15))))
-          ]),
-        ));
+            margin: EdgeInsetsDirectional.only(top: 50),
+            child: Column(
+              children: [
+                Text("Upload a photo?",
+                    style: TextStyle(
+                        fontSize: 25,
+                        color: Colors.black,
+                        fontWeight: FontWeight.w700)),
+
+                // Currently this container is helping with centering the page
+                Container(
+                    margin: EdgeInsetsDirectional.only(top: 25),
+                    child: Row(
+                      children: [
+                        // Expanded(child: TextInputWidget()),
+                      ],
+                    )),
+                Container(
+                    margin: EdgeInsetsDirectional.only(top: 50),
+                    child: ElevatedButton(
+                        onPressed: this.submitPhoto,
+                        child: Text("Update", style: TextStyle(fontSize: 15)),
+                        style: ElevatedButton.styleFrom(
+                            primary: Colors.black,
+                            onPrimary: Colors.white,
+                            shadowColor: Colors.black,
+                            padding: EdgeInsets.fromLTRB(75, 15, 75, 15))))
+              ],
+            )));
   }
 }
